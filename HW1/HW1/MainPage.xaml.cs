67b8040c06944f438a831b97dab0be3412cf002a
庄 清惠 <zhuangqhc@gmail.com>
2016-02-28 11:46:14 +0800

    advanced version

--- HW1/HW1/MainPage.xaml.cs
+++ HW1/HW1/MainPage.xaml.cs
@@ -20,70 +20,87 @@ namespace HW1 {
   /// 可用于自身或导航至 Frame 内部的空白页。
   /// </summary>
   public sealed partial class MainPage : Page {
-    public MainPage() {
-      this.InitializeComponent();
-    }
-
-    interface Animal {
-      string saying();
-    }
-
-    class Pig : Animal {
-      public string saying() {
-        return "I am a pig";
-      }
-    }
-
-    class Dog : Animal {
-      public string saying() {
-        return "I am a dog";
-      }
-    }
-
-    class Cat : Animal {
-      public string saying() {
-        return "I am a cat";
-      }
-    }
-
     private Cat cat;
     private Dog dog;
     private Pig pig;
 
-    private void confirm_Click(object sender, RoutedEventArgs e) {
-      string animalName = namePicker.Text;
-      if (animalName == "cat") {
-        cat = new Cat();
-        board.Text += "cat: " + cat.saying() + "\n";
-      } else if (animalName == "dog") {
-        dog = new Dog();
-        board.Text += "dog: " + dog.saying() + "\n";
-      } else if (animalName == "pig") {
-        pig = new Pig();
-        board.Text += "pig: " + pig.saying() + "\n";
-      }
-      namePicker.Text = string.Empty;
+    public MainPage() {
+      this.InitializeComponent();
+      cat = new Cat(board);
+      dog = new Dog(board);
+      pig = new Pig(board);
     }
 
-    private void namePicker_TextChanged(object sender, TextChangedEventArgs e) {
+    private delegate void AnimalSaying(object sender, EventArgs e);
+    private event AnimalSaying Say;
 
+    interface Animal {
+      void saying(object sender, EventArgs e);
+    }
+
+    class Pig : Animal {
+      TextBlock words;
+
+      public Pig(TextBlock w) {
+        this.words = w;
+      }
+      public void saying(object sender, EventArgs e) {
+        this.words.Text += "pig: I am a pig\n"; 
+      }
+    }
+
+    class Dog : Animal {
+      TextBlock words;
+
+      public Dog(TextBlock w) {
+        this.words = w;
+      }
+      public void saying(object sender, EventArgs e) {
+        this.words.Text += "dog: I am a dog\n";
+      }
+    }
+
+    class Cat : Animal {
+      TextBlock words;
+
+      public Cat(TextBlock w) {
+        this.words = w;
+      }
+      public void saying(object sender, EventArgs e) {
+        this.words.Text += "cat: I am a cat\n";
+      }
+    }
+
+    private void confirm_Click(object sender, RoutedEventArgs e) {
+      string animalName = namePicker.Text.ToLower();
+      namePicker.Text = string.Empty;
+      if (animalName == "cat") {
+        Say = new AnimalSaying(cat.saying);
+      } else if (animalName == "dog") {
+        Say = new AnimalSaying(dog.saying);
+      } else if (animalName == "pig") {
+        Say = new AnimalSaying(pig.saying);
+      } else {
+        return;
+      }
+      Say(this, new EventArgs());
     }
 
     private void saying_Click(object sender, RoutedEventArgs e) {
       Random rd = new Random();
       int character = rd.Next(3);
       if (character == 0) {
-        cat = new Cat();
-        board.Text += "cat: " + cat.saying() + "\n";
+        Say = new AnimalSaying(cat.saying);
       } else if (character == 1) {
-        dog = new Dog();
-        board.Text += "dog: " + dog.saying() + "\n";
+        Say = new AnimalSaying(dog.saying);
       } else if (character == 2) {
-        pig = new Pig();
-        board.Text += "pig: " + pig.saying() + "\n";
-      } else {
-        board.Text += character + "\n";
+        Say = new AnimalSaying(pig.saying);
       }
+      Say(this, new EventArgs());
+    }
+
+    private void namePicker_TextChanged(object sender, TextChangedEventArgs e) {
+
     }
   }
 
